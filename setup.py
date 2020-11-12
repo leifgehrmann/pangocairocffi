f2ef488a85bd2784776463ba72a6d56308c090c6
Leif Gehrmann <leif.gehrmann@gmail.com>
2020-11-12 18:25:26 +0000

    Removed unnecessary setuptools dep

--- setup.py
+++ setup.py
@@ -1,9 +1,6 @@
 import sys
-from setuptools import setup
 
 if sys.version_info.major < 3:
     raise RuntimeError(
         'pangocairocffi does not support Python 2.x. Please use Python 3.'
     )
-
-setup()
